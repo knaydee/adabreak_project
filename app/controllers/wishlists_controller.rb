de1b16a192319e8bdcef43d8bac74d4903308d22
Meighan Rasley <meighanr@gmail.com>
2015-11-26 14:40:29 -0800

    Add button to add item to wishlist, _form.html.erb for new and edit wishlist forms, checkbox for bought

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -7,5 +7,48 @@ class WishlistsController < ApplicationController
     @wishlist = @giftee.wishlists
   end
 
+  def show
+    id = params[:giftee_id]
+    @giftee = Giftee.find(id)
+    @title = "Market Vendors"
+    @wishlist = @giftee.wishlists
+  end
+
+  def new
+    @wishlist = Wishlist.new()
+    @action = "create"
+    @title = "Add Item"
+  end
+
+  def create
+    Wishlist.create(account_params[:wishlist])
+    redirect_to "/wishlists"
+  end
+
+  def edit
+    id = params[:giftee_id]
+    @giftee = Giftee.find(id)
+    @action = "update"
+    @title = "Edit an Item"
+    @wishlist = @giftee.wishlists
+  end
+
+  def update
+    id = params[:id]
+    Wishlist.update(id, account_params[:wishlist])
+    redirect_to "/wishlists/#{id}"
+  end
+
+  def destroy
+    id = params[:id]
+    Wishlist.destroy(id)
+    redirect_to wishlists_path
+  end
+
+  private
+
+  def account_params
+    params.permit(wishtlist:[:item, :price, :bought])
+  end
 
 end
