553f217e71acb5efc8e75d95bf4cbaa34203c8f4
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-02 13:23:21 -0800

    added giftees to user id, made some new views

--- app/models/giftee.rb
+++ app/models/giftee.rb
@@ -1,3 +1,4 @@
 class Giftee < ActiveRecord::Base
   has_many :wishlists
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102205607) do
+ActiveRecord::Schema.define(version: 20160102212218) do
 
   create_table "giftees", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160102205607) do
     t.string   "dislikes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
