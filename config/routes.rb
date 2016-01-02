3667167b19c731e45a69a710fb25e040ce558c26
Riley Spicer <riley.r.spicer@gmail.com>
2016-01-02 12:56:13 -0800

    added user model

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
     resources :wishlists
   end
 
+  resources :users
+
+  get 'sessions/new' => 'sessions#new', as: :login
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
